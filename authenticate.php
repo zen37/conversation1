b5c61d6abbf832eb98df44eaacf764391a9ddb4c
zen <zen@zen>
2024-03-03 13:26:40 -0800

    test

--- authenticate.php
+++ authenticate.php
@@ -2,18 +2,15 @@
 session_start();
 
 echo 'Welcome to page authenticate';
-$_SESSION['favcolor'] = 'green';
-$_SESSION['animal'] = 'cat';
-$_SESSION['time'] = time();
-echo '<br /><a href="page2.php">page 2</a>';
+
 // Or pass along the session ID if needed
 // Change this to your connection info.
 
-/*
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
+/*
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
