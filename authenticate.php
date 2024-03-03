1978054b98ce1a4f03ecb8552dfdd42652d11de9
zen <zen@zen>
2024-03-03 14:03:48 -0800

    test

--- authenticate.php
+++ authenticate.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-echo 'Welcome to page authenticate';
+echo 'Welcome to page authenticate 2';
 
 // Or pass along the session ID if needed
 // Change this to your connection info.
@@ -14,12 +14,12 @@ $DATABASE_NAME = 'phplogin';
 echo $DATABASE_HOST;
 
 // Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-/* 
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+$conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+    // If there is an error with the connection, display a user-friendly message
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+} else {
+    // Connection successful
+    echo "Connected successfully";
 }
-echo "Connected successfully";
-*/
 ?>
