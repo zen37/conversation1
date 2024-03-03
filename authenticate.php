7c7ee76fa8e5d0214f3ee7eae6e370212cc15fb7
zen <zen@zen>
2024-03-03 13:49:01 -0800

    test

--- authenticate.php
+++ authenticate.php
@@ -10,9 +10,12 @@ $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
-/*
+
+echo $DATABASE_HOST;
+
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+/* 
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
