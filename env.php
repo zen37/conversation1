afc54b63bbd7772021fe2214415d4453c3af3909
zen <zen@zen>
2024-03-03 17:57:39 -0800

    env

--- env.php
+++ env.php
@@ -1,5 +1,10 @@
 <?php 
-	foreach ($_ENV as $key => $value) {
-		echo "$key: $value<br>";
-	}
+// Get the keys of environment variables and sort them alphabetically
+$keys = array_keys($_ENV);
+sort($keys);
+
+// Iterate through the sorted keys and display corresponding values
+foreach ($keys as $key) {
+    echo "$key: {$_ENV[$key]}<br>";
+}
 ?>
